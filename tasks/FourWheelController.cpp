f8ddfd9a28a9581beb0cef7e9bb68eb30b9429ce
Ajish Babu <ajish.babu@dfki.de>
2009-12-07 13:38:09 +0100

    PIV controller added for all the four wheels

--- tasks/FourWheelController.cpp
+++ tasks/FourWheelController.cpp
@@ -2,22 +2,6 @@
 
 #include <rtt/NonPeriodicActivity.hpp>
 
-#define	SLIDER_MAX 250			/*   */
-#define	SLIDER_MIN 5			/*   */
-#define	VEL_MAX_PWM 0.25			/*   */
-#define	VEL_MAX_PID 7.0			/*   */
-
-#define	SLIDER_MASTER_VEL_LEFT 1			/*   */
-#define	SLIDER_VEL_LEFT_FRONT 2			/*   */
-#define	SLIDER_VEL_LEFT_REAR 3		/*   */
-
-#define	SLIDER_MASTER_VEL_RIGHT 6			/*   */
-#define	SLIDER_VEL_RIGHT_REAR 4			/*   */
-#define	SLIDER_VEL_RIGHT_FRONT 5			/*   */
-
-#define	SLIDER_ICR 0			/*   */
-
-
 using namespace control;
 
 
@@ -63,110 +47,6 @@ void FourWheelController::updateHook()
             return;
         }
 
-        for (int i = 0; i < 7; i++)
-        {
-                // Reads the slider value from MIN to MAX and scales it to 0 to 1
-                sliderValues[i] = (( oInputCmd.sliderValues[i]- SLIDER_MIN) / (SLIDER_MAX-SLIDER_MIN));
-
-                // Rounds the data outside MIN and MAX
-                if (sliderValues[i] > 1.0)
-                        sliderValues[i] = 1.0;
-                else if (sliderValues[i] < 0.0)
-                        sliderValues[i] = 0.0;
-        }
-
-        if (oInputCmd.devices & controldev::DAI_SliderBox)
-        {
-
-                /* ON state */	
-                if ( oInputCmd.sliderButtons & (1 << 3) )/* Button 4 */
-                {
-                        float vel_max;   // Sets the velocity to be scaled in different modes
-
-                        /* Sets the control mode for each motor */
-                        for (int i=0;i<4;i++)
-                        {
-                                // Sets the mode based on status of Button 3
-                                if (oInputCmd.sliderButtons & (1 << 2))  /* Button 3 */
-                                {
-                                        /* PWM Mode */
-                                        wmcmd.mode[i] = hbridge::DM_PWM;
-                                        vel_max = VEL_MAX_PWM;
-                                }
-                                else
-                                {
-                                        /* PID Mode */
-                                        wmcmd.mode[i] = hbridge::DM_SPEED;
-                                        vel_max = VEL_MAX_PID;
-                                }
-                        }
-
-                        /* Sets the driving mode and calulates the individual velocities*/
-
-                        /* Individual wheel and Master wheel set drive */
-                        if (oInputCmd.sliderButtons & (1 << 1))   /* Button 2 */
-                        {
-                                
-                                wmcmd.target[3] = 2  * (sliderValues[SLIDER_MASTER_VEL_LEFT]  - 0.5) 
-                                        * sliderValues[SLIDER_VEL_LEFT_FRONT]
-                                        * vel_max;
-                                wmcmd.target[0] = 2  * (sliderValues[SLIDER_MASTER_VEL_LEFT]  - 0.5)
-                                        * sliderValues[SLIDER_VEL_LEFT_REAR]
-                                        * vel_max;
-                                wmcmd.target[1] = 2  * (sliderValues[SLIDER_MASTER_VEL_RIGHT] - 0.5)
-                                        * sliderValues[SLIDER_VEL_RIGHT_REAR]
-                                        * vel_max;
-                                wmcmd.target[2] = 2  * (sliderValues[SLIDER_MASTER_VEL_RIGHT] - 0.5)
-                                        * sliderValues[SLIDER_VEL_RIGHT_FRONT]
-                                        * vel_max;
-                        }
-                        /* ICR (Instantaneous Center of Rotation) drive */
-                        else
-                        {
-                                // If slider position is on the left half ... turn left
-                                if ( (sliderValues[SLIDER_ICR] ) <= 0.5 )					
-                                {
-                                        // Master Vel * ICR position * max vel
-                                        wmcmd.target[3] = 2 * (sliderValues[SLIDER_MASTER_VEL_LEFT] - 0.5) *
-                                                4 * (sliderValues[SLIDER_ICR] - 0.25)
-                                                * vel_max;
-                                        wmcmd.target[0] = 2 * (sliderValues[SLIDER_MASTER_VEL_LEFT] - 0.5) *
-                                                4 * (sliderValues[SLIDER_ICR] - 0.25)
-                                                * vel_max;
-
-                                        // Master vel * max vel
-                                        wmcmd.target[1] = 2 * (sliderValues[SLIDER_MASTER_VEL_RIGHT] - 0.5) *
-                                                vel_max;
-                                        wmcmd.target[2] = 2 * (sliderValues[SLIDER_MASTER_VEL_RIGHT] - 0.5) *
-                                                vel_max;
-                                }
-                                // If slider position is on the right half ... turn right
-                                else
-                                {
-                                        // Master vel * max vel					
-                                        wmcmd.target[3] = 2 * (sliderValues[SLIDER_MASTER_VEL_LEFT] - 0.5) *
-                                                vel_max;
-                                        wmcmd.target[0] = 2 * (sliderValues[SLIDER_MASTER_VEL_LEFT] - 0.5) *
-                                                vel_max;
-
-                                        // Master vel + ICR position * max vel
-                                        wmcmd.target[1] = 2 * (sliderValues[SLIDER_MASTER_VEL_RIGHT] - 0.5) *
-                                                4 * (-sliderValues[SLIDER_ICR] + 0.75)
-                                                * vel_max;
-                                        wmcmd.target[2] = 2 * (sliderValues[SLIDER_MASTER_VEL_RIGHT] - 0.5) *
-                                                4 * (-sliderValues[SLIDER_ICR] + 0.75)
-                                                * vel_max;
-                                }
-                        }
-                } else {
-		  //switched off
-		  for(int i = 0; i < 4 ; i++)
-		    {
-		      wmcmd.mode[i] = hbridge::DM_PWM;
-		      wmcmd.target[i] = 0;
-		    }
-		} 
-        }
 
 
 	// Writing out the message
--- tasks/PIVController.cpp
+++ tasks/PIVController.cpp
@@ -3,6 +3,7 @@
 #include <math.h>
 #include <rtt/NonPeriodicActivity.hpp>
 
+#define SAMPLING_TIME 0.001
 
 using namespace control;
 
@@ -10,21 +11,22 @@ RTT::NonPeriodicActivity* PIVController::getNonPeriodicActivity()
 { return dynamic_cast< RTT::NonPeriodicActivity* >(getActivity().get()); }
 
 
-    PIVController::PIVController(std::string const& name, TaskCore::TaskState initial_state)
+	PIVController::PIVController(std::string const& name, TaskCore::TaskState initial_state)
 : PIVControllerBase(name, initial_state)
 {
-    refPos = 0;
-    refVel = 1.0;
+	for(int i=0; i<4; i++)
+	{
+		refPos[i] = 0;
 
-    oPIV.setGains(3.80,0.65,0.07);
-    oPIV.setFeedForwardGain(1.00,1.00);
-    oPIV.setVelSmoothingGain(0.6);
-    oPIV.setSamplingTime(0.001);
-    oPIV.setOutputLimits(-0.6,0.6);
-    oPIV.setIntegratorWindupCoeff(0.06);
-    oPIV.setPositionController(true);
-
-    firstRun = true;
+		oPIV[i].setGains(3.80,0.65,0.07);
+		oPIV[i].setFeedForwardGain(1.00,0.00);
+		oPIV[i].setVelSmoothingGain(0.6);
+		oPIV[i].setSamplingTime(SAMPLING_TIME);
+		oPIV[i].setOutputLimits(-0.6,0.6);
+		oPIV[i].setIntegratorWindupCoeff(0.06);
+		oPIV[i].setPositionController(true);
+	}
+	firstRun = true;
 }
 
 
@@ -38,80 +40,71 @@ RTT::NonPeriodicActivity* PIVController::getNonPeriodicActivity()
 // }
 bool PIVController::startHook()
 {
-    for (int i=0;i<4;i++)
-    {
-	wmcmd.mode[i] 	= hbridge::DM_PWM;
-	wmcmd.target[i] = 0;
-    }
-    return true;
+	for (int i=0;i<4;i++)
+	{
+		wmcmd.mode[i] 	= hbridge::DM_PWM;
+		wmcmd.target[i] = 0;
+	}
+	return true;
 }
 
 void PIVController::updateHook()
 {
-    // This is the hbridge status
-    hbridge::Status status;
-    if (! _status.read(status))
-    {
-	return;
-    }   
+	// This is the hbridge status
+	hbridge::Status status;
+	if (! _status.read(status))
+	{
+		return;
+	}   
 
-    wmcmd.stamp	= DFKI::Time::now();
-    currTime = wmcmd.stamp.seconds + 1e-6 * wmcmd.stamp.microseconds;
+	_four_wheel_cmd.read (refVel);
 
-    if(firstRun)
-    {
-	firstRun = false;
+	wmcmd.stamp	= DFKI::Time::now();
+	currTime = wmcmd.stamp.seconds + 1e-6 * wmcmd.stamp.microseconds;
 
-	prevPos  = status.states[0].position;
+	if(firstRun)
+	{
+		firstRun = false;
+
+		prevTime = currTime;
+		startTime = currTime;
+
+		for(int i=0; i<4; i++)
+		{
+			prevPos[i]  = status.states[i].position;
+			refVelIntegrator[i].init(SAMPLING_TIME, 0.0, prevPos[i]);
+			wmcmd.mode[i] 	= hbridge::DM_PWM;
+		}
+		return;
+	}
+
+	for(int i=0; i<4; i++)
+	{
+		actVel[i] = (status.states[i].position - prevPos[i]) / (currTime - prevTime);
+		refPos[i] = refVelIntegrator[i].update(refVel.target[i]);
+		errPos[i] = refPos[i] - status.states[i].position;
+
+		wmcmd.target[i] = oPIV[i].update(actVel[i], refVel.target[i], errPos[i]);
+
+		prevPos[i]  = status.states[i].position;
+	}
 	prevTime = currTime;
 
-	startTime = currTime;
-        
-	refVelIntegrator.init(0.001, 0.0, prevPos);
-
-	wmcmd.mode[0] 	= hbridge::DM_PWM;
-	return;
-    }
-
-    time = currTime - startTime;
-    if(time <= 1.0)
-	refVel = 2.5 * time; 
-    else
-	if (time > 1.0 && time <= 2.0)
-	    refVel = 2.5 - 2.5 * (time-1.0);
-	else
-	    refVel = 0.0;
-
-    refVel = 2.0+0.5*sin(8.0*time);
-//
-//    refVel = 4.0;
-
-    actVel = (status.states[0].position - prevPos) / (currTime - prevTime);
-
-    refPos = refVelIntegrator.update(refVel);
-    errPos = refPos - status.states[0].position;
-    wmcmd.target[0] = oPIV.update(actVel, refVel, errPos);
-
-    prevPos  = status.states[0].position;
-    prevTime = currTime;
-//
-//    wmcmd.mode[0] = hbridge::DM_SPEED;
-//    wmcmd.target[0] = refVel;
-    // Writing out the message
-    _cmd_out.write(wmcmd);
+	// Writing out the message
+	_cmd_out.write(wmcmd);
 }
 
 // void PIVController::errorHook()
 // {
 // }
- void PIVController::stopHook()
- {
-    for (int i=0;i<4;i++)
-    {
-	wmcmd.mode[i] 	= hbridge::DM_PWM;
-	wmcmd.target[i] = 0;
-    }
- }
+void PIVController::stopHook()
+{
+	for (int i=0;i<4;i++)
+	{
+		wmcmd.mode[i] 	= hbridge::DM_PWM;
+		wmcmd.target[i] = 0;
+	}
+}
 // void PIVController::cleanupHook()
 // {
 // }
