4c965e95e3cb0dd8405671211ab3ef930e7e0724
Sylvain Joyeux <ajish.babu@dfki.de>
2009-12-10 13:45:01 +0100

    connects FourWheelCommand to simpleCommand
    - writes the old simpleCommand output if no new data is available

--- tasks/FourWheelController.cpp
+++ tasks/FourWheelController.cpp
@@ -12,16 +12,7 @@ RTT::NonPeriodicActivity* FourWheelController::getNonPeriodicActivity()
 FourWheelController::FourWheelController(std::string const& name, TaskCore::TaskState initial_state)
     : FourWheelControllerBase(name, initial_state)
 {
-    for(int i = 0; i < 4 ; i++)
-    {
-        wmcmd.mode[i] = hbridge::DM_PWM;
-        wmcmd.target[i] = 0;
-    }
 }
-
-
-
-
 
 /// The following lines are template definitions for the various state machine
 // hooks defined by Orocos::RTT. See FourWheelController.hpp for more detailed
@@ -31,34 +22,55 @@ FourWheelController::FourWheelController(std::string const& name, TaskCore::Task
 // {
 //     return true;
 // }
-// bool FourWheelController::startHook()
-// {
-//     return true;
-// }
+
+bool FourWheelController::startHook()
+{
+    for(int i = 0; i < 4 ; i++)
+    {
+        wmcmd.mode[i] = hbridge::DM_PWM;
+        wmcmd.target[i] = 0;
+    }
+    return true;
+}
 
 void FourWheelController::updateHook()
 {
-        controldev::RawCommand oInputCmd;
+        controldev::FourWheelCommand oInputCmd;
         float sliderValues[7];
-        if (!_raw_cmd_in.read(oInputCmd))
+        if (!_four_wheel_cmd.read(oInputCmd))
         {
             // No data on input, send last command on output
             _cmd_out.write(wmcmd);
             return;
         }
 
+        for (int i = 0; i < 4; ++i)
+        {
+            hbridge::DRIVE_MODE hbridge_mode = hbridge::DM_UNINITIALIZED;
+            if (oInputCmd.mode[i] == controldev::MODE_PWM)
+                hbridge_mode = hbridge::DM_PWM;
+            else if (oInputCmd.mode[i] == controldev::MODE_SPEED)
+                hbridge_mode = hbridge::DM_SPEED;
 
+            wmcmd.mode[i] = hbridge_mode;
+            wmcmd.target[i] = oInputCmd.target[i];
+        }
 
-	// Writing out the message
 	_cmd_out.write(wmcmd);
 }
 
 // void FourWheelController::errorHook()
 // {
 // }
-// void FourWheelController::stopHook()
-// {
-// }
+void FourWheelController::stopHook()
+{
+    for(int i = 0; i < 4 ; i++)
+    {
+        wmcmd.mode[i] = hbridge::DM_PWM;
+        wmcmd.target[i] = 0;
+    }
+    _cmd_out.write(wmcmd);
+}
 // void FourWheelController::cleanupHook()
 // {
 // }
