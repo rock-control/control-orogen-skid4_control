efc78923a20c3e02606ae866734bac380ce9986d
Janosch Machowinski <janosch.machowinski@dfki.de>
2011-05-18 18:27:23 +0200

    added experimental soft turn controller
    This controller is meant to turn in a softer way, as
    it generates a forward and backward movement while
    using small steering angles.

---
+++ tasks/SoftTurnController.cpp
@@ -0,0 +1,230 @@
+/* Generated from orogen/lib/orogen/templates/tasks/Task.cpp */
+
+#include "SoftTurnController.hpp"
+#include "PID.hpp"
+
+using namespace skid4_control;
+
+SoftTurnController::SoftTurnController(std::string const& name)
+    : SoftTurnControllerBase(name)
+{
+}
+
+SoftTurnController::~SoftTurnController()
+{
+}
+
+
+
+/// The following lines are template definitions for the various state machine
+// hooks defined by Orocos::RTT. See SoftTurnController.hpp for more detailed
+// documentation about them.
+
+bool SoftTurnController::configureHook()
+{
+    if (! SoftTurnControllerBase::configureHook())
+        return false;
+    
+    double m_radius = _wheel_radius.get();
+    double m_track_width = _track_width.get();
+    if (m_radius <= 0 || m_track_width <= 0)
+    {
+        RTT::log(RTT::Error) << "wrong radius and/or track_width parameters" << RTT::endlog();
+        return false;
+    }
+    
+    turnSpeed = _turnSpeed.get();
+    translationalSpeed = _translationalSpeed.get();
+    state = FORWARD;
+    ///maximum allowed displacement while turning
+    turnVariance = _turnVariance.get();
+
+    startStatus.resize(4);
+
+    Kp = _P.get();
+    Ki = _I.get();
+    Kd = _D.get();
+    N = _N.get();
+    B = _B.get();
+    Tt = _Tt.get();
+    YMin = -_MinMax.get();
+    YMax = _MinMax.get();
+    leftPid.setParallelCoefficients(Kp, Ki, Kd, N, B, Tt, YMin, YMax);
+    rightPid.setParallelCoefficients(Kp, Ki, Kd, N, B, Tt, YMin, YMax);
+    
+    return true;
+}
+// bool SoftTurnController::startHook()
+// {
+//     if (! SoftTurnControllerBase::startHook())
+//         return false;
+//     return true;
+// }
+
+
+void currentController(double input)
+{
+    
+}
+
+
+double SoftTurnController::leftController(double wantedSpeed, const base::actuators::Status &status)
+{    
+    static double lastOutput = 0.0;
+    const double timeSinceLastMeasurement = status.index - lastStatus.index * 0.001;
+    if(timeSinceLastMeasurement == 0.0)
+        return lastOutput;
+    
+    const double curSpeed = (status.states[base::actuators::WHEEL4_FRONT_LEFT].positionExtern - status.states[base::actuators::WHEEL4_FRONT_LEFT].positionExtern 
+                            + status.states[base::actuators::WHEEL4_REAR_LEFT].positionExtern - status.states[base::actuators::WHEEL4_REAR_LEFT].positionExtern)
+                            / 2.0 / timeSinceLastMeasurement;
+    
+
+    const double output = leftPid.update(curSpeed, wantedSpeed, timeSinceLastMeasurement);
+    lastOutput = output;
+    
+    return output;
+}
+
+double SoftTurnController::rightController(double wantedSpeed, const base::actuators::Status &status)
+{
+    static double lastOutput = 0.0;
+    const double timeSinceLastMeasurement = status.index - lastStatus.index * 0.001;
+    if(timeSinceLastMeasurement == 0.0)
+        return lastOutput;
+    
+    const double curSpeed = (status.states[base::actuators::WHEEL4_FRONT_RIGHT].positionExtern - status.states[base::actuators::WHEEL4_FRONT_RIGHT].positionExtern 
+                            + status.states[base::actuators::WHEEL4_REAR_RIGHT].positionExtern - status.states[base::actuators::WHEEL4_REAR_RIGHT].positionExtern) 
+                            / 2.0 / timeSinceLastMeasurement;
+    
+
+    const double output = rightPid.update(curSpeed, wantedSpeed, timeSinceLastMeasurement);
+    lastOutput = output;
+    
+    return output;    
+}
+
+void SoftTurnController::updateHook()
+{
+    SoftTurnControllerBase::updateHook();
+    
+    double pointTurnLimit = 0.1;
+    double stepLimit = 0.01;
+
+    static double lastSpeed = 0;
+    static base::Time changeTime;
+
+    base::actuators::Status status;
+    if(_status.readNewest(status, true) == RTT::NoData)
+	return;
+
+    // This is the user's command
+    base::MotionCommand2D cmd_in;
+    if (_motion_command.readNewest(cmd_in, true) == RTT::NoData)
+    {
+        cmd_in.translation = 0;
+        cmd_in.rotation    = 0;
+    }
+        
+    //let's allways turn
+    if(true || fabs(cmd_in.rotation) > pointTurnLimit)
+    {
+        //hack
+        cmd_in.rotation = 1.0;
+        cmd_in.rotation = turnSpeed * fabs(cmd_in.rotation) / cmd_in.rotation;
+
+	static bool switchState = false;
+	for(int i = 0; i < 4; i++)
+	{
+	    const double diff = fabs(startStatus.states[i].positionExtern - status.states[i].positionExtern);
+	    //we assume zero slip
+	    if(diff * _wheel_radius.get()  > _turnVariance.get() )
+	    {
+		//we moved the maximum variance in one direction, reverse
+		switchState = true;
+		break;
+	    }
+	}
+	
+	if(!switchState)
+	{
+        switch(state)
+        {
+            case FORWARD:
+                cmd_in.translation = translationalSpeed;		
+                break;
+                
+            case BACKWARD:
+                cmd_in.translation = -translationalSpeed;		
+                break;
+        }
+	}
+	else 
+	{
+	    if(changeTime.isNull())
+		changeTime = base::Time::now();
+
+	    if(base::Time::now() - changeTime > base::Time::fromSeconds(0.5) )
+	    {
+		startStatus = status;
+		if(state == FORWARD)
+		    state = BACKWARD;
+		else
+		    state = FORWARD;
+
+		std::cout << "Switched State" << std::endl;
+		changeTime = base::Time();
+		switchState = false;
+	    }
+	}
+    }
+
+    double newSpeed = 0;
+    if(cmd_in.translation < 0)
+    {
+	newSpeed = lastSpeed - stepLimit;
+	if(newSpeed < cmd_in.translation)
+	    newSpeed = cmd_in.translation;
+    }
+    else
+    {
+	newSpeed = lastSpeed + stepLimit;
+	if(newSpeed > cmd_in.translation)
+	    newSpeed = cmd_in.translation;
+    }
+
+    lastSpeed = newSpeed;
+    cmd_in.translation = newSpeed;
+
+    std::cout << "cmd_in.translation " << cmd_in.translation << "cmd_in.rotation " << cmd_in.rotation << std::endl; 
+    double fwd_velocity = cmd_in.translation / _wheel_radius.get();
+    double differential = cmd_in.rotation * _track_width.get() / _wheel_radius.get();
+    
+    double leftSpeed;
+    double rightSpeed;
+    
+    m_cmd.mode[0] = m_cmd.mode[1] =
+        m_cmd.mode[2] = m_cmd.mode[3] = base::actuators::DM_SPEED;
+
+    
+    m_cmd.target[base::actuators::WHEEL4_FRONT_LEFT]  = fwd_velocity - differential;
+    m_cmd.target[base::actuators::WHEEL4_REAR_LEFT]   = fwd_velocity - differential;
+    m_cmd.target[base::actuators::WHEEL4_FRONT_RIGHT] = fwd_velocity + differential;
+    m_cmd.target[base::actuators::WHEEL4_REAR_RIGHT]  = fwd_velocity + differential;
+    m_cmd.time = base::Time::now();
+
+    _simple_command.write(m_cmd);
+}
+// void SoftTurnController::errorHook()
+// {
+//     SoftTurnControllerBase::errorHook();
+// }
+// void SoftTurnController::stopHook()
+// {
+//     SoftTurnControllerBase::stopHook();
+// }
+// void SoftTurnController::cleanupHook()
+// {
+//     SoftTurnControllerBase::cleanupHook();
+// }
+
---
+++ tasks/SoftTurnController.hpp
@@ -0,0 +1,110 @@
+/* Generated from orogen/lib/orogen/templates/tasks/Task.hpp */
+
+#ifndef SKID4_CONTROL_SOFTTURNCONTROLLER_TASK_HPP
+#define SKID4_CONTROL_SOFTTURNCONTROLLER_TASK_HPP
+
+#include "skid4_control/SoftTurnControllerBase.hpp"
+#include <PID.hpp>
+
+namespace skid4_control {
+    class SoftTurnController : public SoftTurnControllerBase
+    {
+	friend class SoftTurnControllerBase;
+    protected:
+        enum State {
+            FORWARD,
+            BACKWARD, 
+        };
+        
+        double turnSpeed;
+        double translationalSpeed;
+        State state;
+        ///maximum allowed displacement while turning
+        double turnVariance;
+
+        ///PID Configuration
+        double Kp;
+        double Ki;
+        double Kd; 
+        double N;
+        double B; 
+        double Tt;
+        double YMin; 
+        double YMax;
+
+        
+        motor_controller::PID leftPid;
+        motor_controller::PID rightPid;
+        base::actuators::Status lastStatus;
+
+	base::actuators::Status startStatus;
+
+        double leftController(double wantedSpeed, const base::actuators::Status &status);
+        double rightController(double wantedSpeed, const base::actuators::Status &status);
+
+    public:
+	SoftTurnController(std::string const& name = "skid4_control::SoftTurnController");
+
+	~SoftTurnController();
+
+        /** This hook is called by Orocos when the state machine transitions
+         * from PreOperational to Stopped. If it returns false, then the
+         * component will stay in PreOperational. Otherwise, it goes into
+         * Stopped.
+         *
+         * It is meaningful only if the #needs_configuration has been specified
+         * in the task context definition with (for example):
+         *
+         *   task_context "TaskName" do
+         *     needs_configuration
+         *     ...
+         *   end
+         */
+        bool configureHook();
+
+        /** This hook is called by Orocos when the state machine transitions
+         * from Stopped to Running. If it returns false, then the component will
+         * stay in Stopped. Otherwise, it goes into Running and updateHook()
+         * will be called.
+         */
+        // bool startHook();
+
+        /** This hook is called by Orocos when the component is in the Running
+         * state, at each activity step. Here, the activity gives the "ticks"
+         * when the hook should be called.
+         *
+         * The error(), exception() and fatal() calls, when called in this hook,
+         * allow to get into the associated RunTimeError, Exception and
+         * FatalError states. 
+         *
+         * In the first case, updateHook() is still called, and recover() allows
+         * you to go back into the Running state.  In the second case, the
+         * errorHook() will be called instead of updateHook(). In Exception, the
+         * component is stopped and recover() needs to be called before starting
+         * it again. Finally, FatalError cannot be recovered.
+         */
+        void updateHook();
+
+        /** This hook is called by Orocos when the component is in the
+         * RunTimeError state, at each activity step. See the discussion in
+         * updateHook() about triggering options.
+         *
+         * Call recovered() to go back in the Runtime state.
+         */
+        // void errorHook();
+
+        /** This hook is called by Orocos when the state machine transitions
+         * from Running to Stopped after stop() has been called.
+         */
+        // void stopHook();
+
+        /** This hook is called by Orocos when the state machine transitions
+         * from Stopped to PreOperational, requiring the call to configureHook()
+         * before calling start() again.
+         */
+        // void cleanupHook();
+    };
+}
+
+#endif
+
